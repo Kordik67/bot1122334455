c49d1279f9f5bac425bd6e1c9a4800ec2d3e4f61
Kordik67 <44140877+Kordik67@users.noreply.github.com>
2018-10-14 18:08:05 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,106 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const settings = require('./settings.json');
+const chalk = require('chalk');
+const fs = require('fs');
+const moment = require('moment');
+var anti_spam = require("discord-anti-spam");
+require('./util/eventLoader')(client);
+require('events').EventEmitter.prototype._maxListeners = 100;
+
+const log = message => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+};
+
+client.on('ready', () => {
+//PLAYING,STREAMING,LISTENING,WATCHING
+client.user.setActivity('dev by Kordik#1419 |~help', {type: "PLAYING"})
+}); 
+
+anti_spam(client, {
+  warnBuffer: 3, //Maximum amount of messages allowed to send in the interval time before getting warned.
+  maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned.
+  interval: 1000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
+  warningMessage: "stop spamming or I'll whack your head off.", // Warning message send to the user indicating they are going to fast.
+  banMessage: "has been banned for spamming, anyone else?", // Ban message, always tags the banned user in front of it.
+  maxDuplicatesWarning: 7,// Maximum amount of duplicate messages a user can send in a timespan before getting warned
+  maxDuplicatesBan: 10, // Maximum amount of duplicate messages a user can send in a timespan before getting banned
+  deleteMessagesAfterBanForPastDays: 7 // Delete the spammed messages after banning for the past x days.
+});
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./commands/', (err, files) => {
+  if (err) console.error(err);
+  log(`Loading a total of ${files.length} commands.`);
+  files.forEach(f => {
+    const props = require(`./commands/${f}`);
+    log(`Loading Command: ${props.help.name}. 👌`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./commands/${command}`)];
+      const cmd = require(`./commands/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.elevation = message => {
+  /* This function should resolve to an ELEVATION level which
+     is then sent to the command handler for verification*/
+  let permlvl = 0;
+  const mod_role = message.guild.roles.find('name', settings.modrolename || settings.modrolename2);
+  if (mod_role && message.member.roles.has(mod_role.id)) permlvl = 2;
+  const admin_role = message.guild.roles.find('name', settings.adminrolename || settings.adminrolename2);
+  if (admin_role && message.member.roles.has(admin_role.id)) permlvl = 3;
+  if (message.author.id === settings.ownerid || settings.ownerid2) permlvl = 4;
+  return permlvl;
+};
+
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+// client.on('debug', e => {
+//   console.log(chalk.bgBlue.green(e.replace(regToken, 'that was redacted')));
+// });
+
+client.on('warn', e => {
+  console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
+});
+
+client.on('error', e => {
+  console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+});
+
+client.on('message', message => {
+if(message.content.includes('discord.gg')){
+if(message.channel.name !== 'pub' || message.channel.name !== '✉┇serveurs') {
+message.delete()
+message.reply("You can make this only on #pub channel !")
+}
+}
+if(message.content.includes('@everyone')){
+if(message.author.bot){
+let bot = message.author;
+message.guild.member(bot).ban()
+}}
+});
+
+client.login(settings.token);
